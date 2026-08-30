6eeb2162b102a4a766d5a17319c8ee0612c26116
mikey733-A <322579214+mikey733-A@users.noreply.github.com>
2026-08-30 04:11:43 +0300

    Update generate_password_hash.py

--- generate_password_hash.py
+++ generate_password_hash.py
@@ -1,4 +1,4 @@
 from getpass import getpass
 from werkzeug.security import generate_password_hash
-pw=getpass("Admin password: ")
+pw=getpass("Admin password:majed12 ")
 print(generate_password_hash(pw))
